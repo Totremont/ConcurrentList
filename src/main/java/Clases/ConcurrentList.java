4fbf7c943a205fed625d4e25cbddf4ef1c11c25e
Tomas Haulet <tomshaulet@gmail.com>
2024-12-08 09:59:47 -0300

    solo agregue clases

---
+++ src/main/java/Clases/ConcurrentList.java
@@ -0,0 +1,11 @@
+package Clases;
+
+public class ConcurrentList {
+    public Node head;
+
+    public ConcurrentList(){}
+
+    public void add(Object o){}
+    public void remove(Object o){}
+    public Object get(int i){return null;}
+}
---
+++ src/main/java/Clases/Node.java
@@ -0,0 +1,9 @@
+package Clases;
+
+public class Node {
+    public Object item;
+    public int key;
+    public Node next;
+
+    public Node(){}
+}
