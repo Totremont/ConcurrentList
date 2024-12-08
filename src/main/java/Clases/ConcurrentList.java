ed9d355edbcd14488015eb58efeb1ef1de789f17
Tomas Haulet <tomshaulet@gmail.com>
2024-12-08 16:58:54 -0300

    gf1

--- src/main/java/Clases/ConcurrentList.java
+++ src/main/java/Clases/ConcurrentList.java
@@ -2,15 +2,75 @@ package Clases;
 
 public class ConcurrentList {
     /*
-    GRANULARIDAD FINA
-    */
+    * GRANULARIDAD FINA
+    * */
 
     public Node head;
 
-    public ConcurrentList(){}
+    public ConcurrentList(){
+        head = new Node(Integer.MIN_VALUE);
+        head.next = new Node(Integer.MAX_VALUE);
+    }
 
-    public void add(Object o){}
-    public void remove(Object o){}
+    public void add(Object o){
+        int key = o.hashCode();
+        Node pred = null;
+        Node curr = null;
+
+        try{
+            pred = head;
+            pred.lock();
+            curr = pred.next;
+            curr.lock();
+
+            while(curr.key < key){
+                pred.unlock();
+                pred = curr;
+                curr = curr.next;
+                curr.lock();
+            }
+
+            if(key == curr.key) return;
+
+            Node nuevo = new Node(o);
+            nuevo.next = curr;
+            pred.next = nuevo;
+            return;
+
+        }finally {
+            if(pred != null) pred.unlock();
+            if(curr != null) curr.unlock();
+        }
+    }
+
+    public void remove(Object o){
+        int key = o.hashCode();
+        Node pred = null;
+        Node curr = null; // Pred: anterior, curr: actual
+
+        try{
+            pred = head;
+            pred.lock();
+            curr = pred.next;
+            curr.lock();
+
+            while(curr.key < key){
+                pred.unlock();
+                pred = curr.next;
+                curr = pred.next;
+                curr.lock();
+            }
+
+            if(key == curr.key){
+                pred.next = curr.next;
+            }
+        }finally {
+            if(pred != null) pred.unlock();
+            if(curr != null) curr.unlock();
+        }
+
+    }
+
     public Object get(int i){return null;}
 
 }
--- src/main/java/Clases/Node.java
+++ src/main/java/Clases/Node.java
@@ -1,12 +1,28 @@
 package Clases;
 
+import java.util.concurrent.Semaphore;
+
 public class Node {
+    /*
+    * GRANULARIDAD FINA
+    * */
+
     public Object item;
     public int key;
     public Node next;
+    private Semaphore lock;
 
-    public Node(){}
+    public Node(Object item){
+        this.item = item;
+        this.key = item.hashCode();
+        lock = new Semaphore(1, true);
+    }
 
-    public void lock(){}
-    public void unlock(){}
+    public void lock(){
+        lock.acquireUninterruptibly();
+    }
+
+    public void unlock(){
+        lock.release();
+    }
 }
