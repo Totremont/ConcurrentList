27356259453f89f7ba844b5af56d19fc6bd079f5
Izaguirre, Ezequiel Matías <ezequielmizaguirre@gmail.com>
2024-12-08 17:45:33 -0300

    Reescribe la función para obtener datos del usuario y la hace más robusta.

--- src/main/java/utn/totremont/State.java
+++ src/main/java/utn/totremont/State.java
@@ -53,44 +53,59 @@ public class State
         text.append("[2] - MODIFICAR escenario actual.\n");
         text.append("[3] - SALIR.\n");
         System.out.println(text);
-        int option = 0;
+        int[] option;
         do {
             System.out.print("\rEscriba el número: ");
-            option = input.nextInt();
-        } while (option < 1 || option > 3);
-        return option;
+            option = getOrParseInput(true,input);
+        } while (option == null || option[0]  < 1 || option[0] > 3);
+        return option[0];
     }
 
     private void changeParameters()
     {
         System.out.flush();
         System.out.println("\n==== TP Programación Concurrente | Modificar ====\n");
-        int option;
-        do {
-            System.out.printf("\rHilos [Actual: %d | Max: 15]: ",threadCount);
-            option = input.nextInt();
-        } while (option < 1 || option > 25);
-        threadCount = option;
+        int[] option;
 
         do {
             System.out.printf("\rOperaciones por hilo [Actual: %d | Max: 20]: ", thOpCount);
-            option = input.nextInt();
-        } while (option < 1 || option > 20);
-        thOpCount = option;
+            option = getOrParseInput(true,input);
+        } while (option == null || option[0] < 1 || option[0] > 20);
+        thOpCount = option[0];
 
         System.out.printf("Proporción (ADD,REMOVE) [Actual: (%d,%d)]\n", opShare[0],opShare[1]);
-        int[] values = new int[2];
+        int[] values;
         do {
             System.out.print("\rEscribir en formato <%,%> (ej: 25,75): ");
-            String[] aux = input.nextLine().split(",");
-            if(aux.length >= 2)
-            {
-                values[0] = Integer.parseInt(aux[0]);
-                values[1] = Integer.parseInt(aux[1]);
-            }
+            values = getOrParseInput(false,input);
 
-        } while (values[0] + values[1] != 100);
+        } while (values == null || (values[0] + values[1]) != 100);
         opShare = values;
 
+        final int minThreads = values[0] < values[1] ? 100 / values[0] : 100 / values[1];
+
+        System.out.printf("Considere que necesita como mínimo %d hilo(s) para poder conseguir la proporción indicada.\n",minThreads);
+
+        do {
+            System.out.printf("\rHilos [Actual: %d | Max: 15]: ",threadCount);
+            option = getOrParseInput(true,input);
+        } while (option == null || option[0] < 1 || option[0] > 25);
+        threadCount = option[0];
+
+    }
+
+    //Este es un wrapper para manejar inputs inválidos de forma cómoda, sin tener que usar try-catchs.
+    // Get : Obtiene un int (index 0) ; Parse : Obtiene 2 ints <%,&> (Proporción ADD,REMOVE).
+    private int[] getOrParseInput(boolean get, Scanner in)
+    {
+        try {
+            String line = in.nextLine();    //Consume toda la línea y evita que se acumulen tokens.
+            if (get) return new int[]{Integer.parseInt(line)};
+            else
+            {
+                String[] aux = line.split(",");
+                return new int[]{Integer.parseInt(aux[0]), Integer.parseInt(aux[1]) };
+            }
+        } catch(Exception e) {return null;}
     }
 }
