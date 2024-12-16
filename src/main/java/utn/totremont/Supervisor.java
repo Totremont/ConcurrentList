08156c6cfbced1c1bde32688bcfa4e05adf736ad
Izaguirre, Ezequiel Matías <ezequielmizaguirre@gmail.com>
2024-12-15 21:18:27 -0300

    Implementa OptimisticStrategy y añade resumen de resultados al finalizar.

--- src/main/java/utn/totremont/Supervisor.java
+++ src/main/java/utn/totremont/Supervisor.java
@@ -1,13 +1,24 @@
 package utn.totremont;
 
+import utn.totremont.strategy.Strategy;
 import utn.totremont.worker.Worker;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 // Receives results from threads
 public class Supervisor
 {
     private final HashSet<Worker> workers = new HashSet<>();
+    private final ArrayList<Strategy> strategies = new ArrayList<>();
+    //Strategy being executed now
+    private int strategyIndex = 0;
+    private Instant lastRun;
+    private int workersFinish = 0;
+    private final ArrayList<Double> times = new ArrayList();
     private final StringBuilder results = new StringBuilder();
     private final LinkedList list;
 
@@ -21,43 +32,83 @@ public class Supervisor
         if(workers.add(worker)) worker.subscribe(this::onReceive);
     }
 
+    public void clear()
+    {
+        workers.clear();
+        strategies.clear();
+        times.clear();
+        strategyIndex = 0;
+        workersFinish = 0;
+    }
+
+    public void setStrategies(List<Strategy> strategies)
+    {
+        this.strategies.addAll(strategies);
+    }
+
+
     // Execute scene
     //Synchronized Evita race conditions con onReceive al modificar el hashset.
     public synchronized void execute()
     {
-        results.append("\n==== Ejecución del escenario ====\n").append(String.format("Creando %d hilo(s)",workers.size()));
-        if(workers.isEmpty())
+        results.append("\n\n==== Ejecución del escenario ====\n");
+        if(workers.isEmpty() || strategies.isEmpty())
         {
-            results.append("\n=== No hay hilos por ejecutar. Adiós. ===");
+            results.append("\n=== No hay hilos o estrategias por ejecutar. Adiós. ===");
             System.out.println(results);
             System.exit(0);
         }
+        forward();
+    }
+
+    private synchronized void forward()
+    {
+        Strategy strategy = this.strategies.get(strategyIndex);
+        this.list.setStrategy(strategies.get(strategyIndex));
+        lastRun = Instant.now();
+        results.append(String.format("\n=== Ejecutando estrategia: %s en lista nueva. ===\n",strategy.name()));
+        results.append(String.format("Creando %d hilo(s)\n",workers.size()));
         workers.forEach(it -> new Thread(it).start());
     }
 
     //Receiving results from threads
     public synchronized void onReceive(Worker worker, String result)
     {
-        results.append(String.format("\n\n[Hilo %d dice]\n",worker.getId())).append(result);
-        workers.remove(worker);
-        if(workers.isEmpty())  // Execution ended
+        results.append(String.format("\n[Hilo %d dice]\n",worker.getId())).append(result);
+        workersFinish++;
+        times.add(strategyIndex, Duration.between(lastRun,Instant.now()).getNano() / 1e6);
+        if(workersFinish == workers.size())  // Execution ended
         {
-            results.append("\n\n=== Todos los hilos han terminado. ===\n");
-            results.append("\n\nEstado de la lista:\n");
-            Node node = list.getHEAD().getNext();
-            int index = 0;
-            while(node != null)
+            if((strategyIndex + 1) == strategies.size())
             {
-                results.append(String.format("[Index: %d | Valor: %d]\n", index, node.getKey()));
-                index++;
-                node = node.getNext();
+                results.append("\n\n=== La ejecución ha concluído. ===\n");
+                results.append("Resumen:\n");
+                for (int i = 0; i < strategies.size(); i++) {
+                    results.append("Estrategia: ")
+                            .append(strategies.get(i).name())
+                            .append(" --> ").append(String.format("%1.3f milisegundos\n", times.get(i)));
+                }
+                System.out.println(results);
+                System.out.println("\nPresionar tecla para continuar.");
             }
-            System.out.println(results);
-            System.out.println("\nPresionar tecla para continuar.");
+            else
+            {
+                results.append("\n\n=== Todos los hilos han terminado. ===\n");
+                results.append("Estado de la lista:\n");
+                Node node = this.list.getFirst();
+                int index = 0;
+                if(node == null) results.append("Vacía.\n");
+                else do
+                {
+                    index++;
+                    results.append(String.format("[Elemento: %d | Valor: %d]\n", index, node.getKey()));
+                } while((node = node.getNext()) != null);
+                strategyIndex++;
+                workersFinish = 0;
+                forward();
+            }
 
         }
     }
-
-    //Evita race conditions con onReceive
 
 }
---
+++ src/main/java/utn/totremont/strategy/OptimisticSynchronizationStrategy.java
@@ -0,0 +1,143 @@
+package utn.totremont.strategy;
+
+import utn.totremont.Node;
+import utn.totremont.utils.Pair;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class OptimisticSynchronizationStrategy implements Strategy{
+
+    @Override
+    public Node addNode(Object value, Node HEAD) {
+
+        int key = value.hashCode();
+
+        while(true){ // La unica forma de salir (agregar nodo) es que se acepte la validacion y se retorne el Node
+
+            OptimisticSynchronizationNode pred = (OptimisticSynchronizationNode) HEAD;
+            OptimisticSynchronizationNode curr = (OptimisticSynchronizationNode) pred.getNext();
+
+            try {
+
+                while(curr.getKey() < key){
+                    pred = curr;
+                    curr = (OptimisticSynchronizationNode) curr.getNext();
+                }
+
+                pred.lock();
+                curr.lock();
+
+                if(validate(pred, curr, HEAD)){
+                    if(curr.getKey() == key) return null;
+                    else {
+                        OptimisticSynchronizationNode node = new OptimisticSynchronizationNode(value, curr);
+                        pred.setNext(node);
+                        return node;
+                    }
+                }
+
+            }finally {
+                pred.unlock();
+                curr.unlock();
+            }
+        }
+
+    }
+
+    @Override
+    public Node removeNode(Object value, Node HEAD) {
+
+        int key = value.hashCode();
+
+        while(true) { // La unica forma de salir (agregar nodo) es que se acepte la validacion y se retorne el Node
+
+            OptimisticSynchronizationNode pred = (OptimisticSynchronizationNode) HEAD;
+            OptimisticSynchronizationNode curr = (OptimisticSynchronizationNode) pred.getNext();
+
+            try {
+
+                while(curr.getKey() < key)
+                {
+                    pred = curr;
+                    curr = (OptimisticSynchronizationNode) curr.getNext();
+                }
+
+                pred.lock();
+                curr.lock();
+
+                if(validate(pred, curr, HEAD)){
+                    if(curr.getKey() != key) return null;
+                    else {
+                        pred.setNext(curr.getNext());
+                        return curr;
+                    }
+                }
+
+            }finally {
+
+                pred.unlock();
+                curr.unlock();
+            }
+        }
+    }
+
+    @Override
+    public Boolean contains(Object value, Node HEAD) {
+        return null;
+    }
+
+    @Override
+    public String name() {
+        return "Optimist strategy";
+    }
+
+    @Override
+    public Node getHEAD() {
+        return new OptimisticSynchronizationNode(Integer.MIN_VALUE, new OptimisticSynchronizationNode(Integer.MAX_VALUE, null));
+    }
+
+    /*private Pair<OptimisticSynchronizationNode> findPosition(Node HEAD, int key){
+
+        OptimisticSynchronizationNode pred = (OptimisticSynchronizationNode) HEAD;
+        OptimisticSynchronizationNode curr = null;
+
+        curr = pred.getNext() != null ? (OptimisticSynchronizationNode) pred.getNext() : null;
+        if(curr != null)
+        {
+            while (curr.getKey() < key)
+            {
+                pred = curr;
+                curr = curr.getNext() != null ? (OptimisticSynchronizationNode) curr.getNext() : null;
+            }
+        }
+        return new Pair<OptimisticSynchronizationNode>(pred,curr);
+    }*/
+
+    // Objetivo: ver que predv.next = currv
+    public Boolean validate(Node predv, Node currv, Node HEAD){
+
+        OptimisticSynchronizationNode node =  (OptimisticSynchronizationNode) HEAD;
+
+        while(node.getKey() <= predv.getKey()){
+            if(node == predv) return node.getNext() == currv;
+
+            node = (OptimisticSynchronizationNode) node.getNext();
+        }
+
+        return false;
+    }
+
+    private class OptimisticSynchronizationNode extends Node {
+
+        private final ReentrantLock lock;
+
+        public OptimisticSynchronizationNode(Object value, Node next) {
+            super(value, next);
+            this.lock = new ReentrantLock();
+        }
+
+        public void lock(){ lock.lock(); }
+        public void unlock(){ lock.unlock(); }
+
+    }
+}
